abe806fa4fdd493e0b9403a0963185d0486695c6
mohammad_saadati <saadati.m.b@gmail.com>
2023-05-28 18:17:46 +0330

    index page reafactor

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,12 @@
-export default function Home() {
+import type { ReactElement } from "react";
+import { NextPageWithLayout } from "./_app";
+import { identifier, LayoutTypes } from "@/components/layouts/layoutIdentifire";
+
+const Home: NextPageWithLayout = () => {
   return <div>home</div>;
-}
+};
+
+Home.getLayout = (page: ReactElement) =>
+  identifier(page, LayoutTypes.dashboard);
+
+export default Home;
