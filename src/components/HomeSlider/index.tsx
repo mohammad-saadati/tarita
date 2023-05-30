d22c42b92a3bfbdfa6b35e083959c5ebaa33c220
mohammad_saadati <saadati.m.b@gmail.com>
2023-05-31 00:04:09 +0330

    add swiper with initial data

--- src/components/HomeSlider/index.tsx
+++ src/components/HomeSlider/index.tsx
@@ -1,20 +1,21 @@
+import Image from "next/image";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 
-const HomeSlider = () => {
+const HomeSlider = ({ slides }) => {
   return (
     <div>
       <Swiper
         spaceBetween={50}
-        slidesPerView={3}
+        slidesPerView={1}
         onSlideChange={() => console.log("slide change")}
         onSwiper={(swiper) => console.log(swiper)}
       >
-        <SwiperSlide>Slide 1</SwiperSlide>
-        <SwiperSlide>Slide 2</SwiperSlide>
-        <SwiperSlide>Slide 3</SwiperSlide>
-        <SwiperSlide>Slide 4</SwiperSlide>
-        ...
+        {slides.map((slide, index) => (
+          <SwiperSlide key={index}>
+            <img src={slide.url} />
+          </SwiperSlide>
+        ))}
       </Swiper>
     </div>
   );
