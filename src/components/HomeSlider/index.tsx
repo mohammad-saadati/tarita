45e6b726827201b78a341384f8fbd319aeb8f2d8
mohammad_saadati <saadati.m.b@gmail.com>
2023-05-31 12:41:25 +0330

    install modules

--- src/components/HomeSlider/index.tsx
+++ src/components/HomeSlider/index.tsx
@@ -1,11 +1,20 @@
 import Image from "next/image";
 import { Swiper, SwiperSlide } from "swiper/react";
+// modules
+import { Navigation, Pagination } from "swiper";
+
+// Import Swiper styles
 import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
 
 const HomeSlider = ({ slides }) => {
   return (
     <div>
       <Swiper
+        modules={[Navigation, Pagination]}
+        pagination
+        navigation
         spaceBetween={50}
         slidesPerView={1}
         onSlideChange={() => console.log("slide change")}
