1993bdd9d8f81db7bc3106beb220943e6f431e4f
mohammad_saadati <saadati.m.b@gmail.com>
2023-06-02 02:06:23 +0330

    swiper props

--- src/components/HomeSlider/index.tsx
+++ src/components/HomeSlider/index.tsx
@@ -1,28 +1,52 @@
 import Image from "next/image";
+//
+import { useState, useEffect } from "react";
+// swiper
 import { Swiper, SwiperSlide } from "swiper/react";
-// modules
+// swiper modules
 import { Navigation, Pagination } from "swiper";
 
-// Import Swiper styles
+// import Swiper styles
 import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
 
 const HomeSlider = ({ slides }) => {
+  const [loading, setLoading] = useState(true);
+  const [loadedImageNumber, setloadedImageNumber] = useState(0);
+
+  const loadedHandler = () => {
+    console.log("test");
+    setloadedImageNumber((prev) => prev + 1);
+  };
+
+  useEffect(() => {
+    if (loadedImageNumber == slides.length) setLoading(false);
+  });
+
   return (
     <div>
       <Swiper
         modules={[Navigation, Pagination]}
         pagination
         navigation
-        spaceBetween={50}
+        spaceBetween={0}
         slidesPerView={1}
         onSlideChange={() => console.log("slide change")}
         onSwiper={(swiper) => console.log(swiper)}
+        placeholder="blur"
       >
         {slides.map((slide, index) => (
           <SwiperSlide key={index}>
-            <img src={slide.url} />
+            <div className="relative h-[300px] w-[1078px]">
+              <Image
+                src={slide.url}
+                fill={true}
+                alt=""
+                className={`object-cover rounded-[10px]`}
+                onLoad={loadedHandler}
+              />
+            </div>
           </SwiperSlide>
         ))}
       </Swiper>
