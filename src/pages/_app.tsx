c4ae9137854a99d6ac2a4d8c5e7679ebad18762e
mohammad_saadati <saadati.m.b@gmail.com>
2023-06-03 15:43:29 +0330

    load fonts with next/font util

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,37 @@
 import { ReactElement, ReactNode } from "react";
 import type { NextPage } from "next";
 import type { AppProps } from "next/app";
+// font
+import localFont from "next/font/local";
 //
 import "@/app/globals.css";
+// icomoon
+import "../../public/fonts/icomoon/style.css";
+
+const fonts = localFont({
+  src: [
+    {
+      path: "../../public/fonts/IranSans/fonts/ttf/IRANSansWeb.ttf",
+      weight: "400",
+    },
+    {
+      path: "../../public/fonts/IranSans/fonts/ttf/IRANSansWeb_UltraLight.ttf",
+      weight: "200",
+    },
+    {
+      path: "../../public/fonts/IranSans/fonts/ttf/IRANSansWeb_Light.ttf",
+      weight: "300",
+    },
+    {
+      path: "../../public/fonts/IranSans/fonts/ttf/IRANSansWeb_Medium.ttf",
+      weight: "500",
+    },
+    {
+      path: "../../public/fonts/IranSans/fonts/ttf/IRANSansWeb_Bold.ttf",
+      weight: "700",
+    },
+  ],
+});
 
 export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
   getLayout?: (page: ReactElement) => ReactNode;
@@ -15,5 +44,9 @@ type AppPropsWithLayout = AppProps & {
 export default function MyApp({ Component, pageProps }: AppPropsWithLayout) {
   const getLayout = Component.getLayout ?? ((page) => page);
 
-  return getLayout(<Component {...pageProps} />);
+  return getLayout(
+    <main className={fonts.className}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
