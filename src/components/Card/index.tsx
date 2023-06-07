af335f457a339970eb2cb6d317e0076bcf746eac
mohammad_saadati <saadati.m.b@gmail.com>
2023-06-08 01:57:00 +0330

    product card component

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -2,14 +2,24 @@ import Image from "next/image";
 import { FC } from "react";
 
 type CardProps = {
-  item: { image: string; title: string };
+  item: { url: string; title: string; price: number; rate: number };
 };
 const Card: FC<CardProps> = ({ item }) => {
   return (
-    <div>
+    <div className="w-[215] h-[248px]">
       <i className="icon-like"></i>
-      <Image src={item.image} fill={true} alt={item.title} />
-      <div>{item.title}</div>
+      <div className="relative w-[160px] h-[150px]">
+        <Image src={item.url} fill={true} alt={item.title} />
+      </div>
+      <div className="mt-[30px] text-[13px] font-medium">{item.title}</div>
+      <div>{item.price}</div>
+      <div className="flex ">
+        <div>قیمت:</div>
+        <div>
+          {item.rate}
+          <span>تومان</span>
+        </div>
+      </div>
     </div>
   );
 };
