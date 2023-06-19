e1409b1529a75fb29e3456aaabff93d936780f0d
mohammad_saadati <saadati.m.b@gmail.com>
2023-06-20 00:49:00 +0330

    fetch data on the server

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,3 +1,17 @@
 export default function Product () {
   return <div>product page</div>
 }
+export async function getServerSideProps(context: GetServerSidePropsContext) {
+  try {
+    const res = await axios.get(`/product/${context.params?.id}`);
+    const { data } = res;
+    console.log("***ddddd*", context.params?.id);
+    return { props: { data } };
+  } catch (error) {
+    console.log(error);
+  }
+
+  return { props: { data: {} } };
+}
+
+export default Product;
