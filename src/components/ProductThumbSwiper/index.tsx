c3f6720c17bfc8d6b6a949be7a5010026e1fc827
mohammad_saadati <saadati.m.b@gmail.com>
2023-06-23 20:06:42 +0330

    workinkg on product sliders

--- src/components/ProductThumbSwiper/index.tsx
+++ src/components/ProductThumbSwiper/index.tsx
@@ -5,21 +5,30 @@ import Slider from "@/components/Slider";
 // styles
 import "swiper/css/free-mode";
 import "swiper/css/thumbs";
+import { Autoplay } from "swiper";
 
+interface photo {
+  url: string;
+  title: string;
+  width: number;
+  height: number;
+}
 interface ProductThumbSwiperProps {
   images: string[];
-  thumbs: string[];
+  thumbs: photo[];
 }
 
 const ProductThumbSwiper: FC<ProductThumbSwiperProps> = ({
   images,
   thumbs,
 }) => {
+  const [thumbsSwiper, setThumbsSwiper] = useState(null);
+
   const renderImages = (slide: string, index: number) => {
     return (
       <div>
         <SwiperSlide>
-          <div className="relative w-[370px] h-[510px] mx-auto">
+          <div className="relative w-[340px] h-[320px] md:w-[370px] md:h-[370px] mx-auto">
             <Image
               src={slide}
               key={index}
@@ -32,11 +41,50 @@ const ProductThumbSwiper: FC<ProductThumbSwiperProps> = ({
       </div>
     );
   };
+  const renderThumbs = (slide: photo, index: number) => {
+    return (
+      <div className="flex justify-center items-center">
+        <SwiperSlide>
+          <div className="flex justify-center items-center">
+            <Image
+              src={slide.url}
+              key={index}
+              alt=""
+              width={slide.width}
+              height={slide.height}
+            />
+          </div>
+        </SwiperSlide>
+      </div>
+    );
+  };
 
   return (
     <>
       <div>
-        <Slider slides={images} renderSlides={renderImages} />
+        <Slider
+          slides={images}
+          renderSlides={renderImages}
+          options={{
+            autoplay: false,
+            spaceBetween: 10,
+            centeredSlides: true,
+            loop: false,
+            thumbs: { swiper: thumbsSwiper },
+          }}
+        />
+        <Slider
+          slides={thumbs}
+          renderSlides={renderThumbs}
+          onSwiper={setThumbsSwiper}
+          options={{
+            autoplay: false,
+            freeMode: true,
+            slidesPerView: 6,
+            loop: false,
+            centerInsufficientSlides: true,
+          }}
+        />
       </div>
     </>
   );
