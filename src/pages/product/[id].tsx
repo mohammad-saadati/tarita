c07df7603bce76ef00e71983c6baac8b0bb64368
mohammad_saadati <saadati.m.b@gmail.com>
2023-06-25 20:15:09 +0330

    add types

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -10,6 +10,12 @@ interface ProductProps {
     title: string;
     mainImages: string[];
     thumbnails: [];
+    cat: string;
+    brand: string;
+    stock: number;
+    features: { title: string; value: number }[];
+    colors: string[];
+    sizes: { title: string; price: number }[];
   };
 }
 
