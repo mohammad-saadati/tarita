e8a605e06857e1fee041cc34c011072cb0feef9c
mohammad_saadati <saadati.m.b@gmail.com>
2023-06-25 20:16:03 +0330

    template

--- src/components/ProductDetail/index.tsx
+++ src/components/ProductDetail/index.tsx
@@ -1,3 +1,5 @@
+import { FC } from "react";
+
 type ProductDetailProps = {
   title: string;
   cat: string;
@@ -7,3 +9,46 @@ type ProductDetailProps = {
   colors: string[];
   sizes: { title: string; price: number }[];
 };
+
+const ProductDetail: FC<ProductDetailProps> = (data) => {
+  return (
+    <div>
+      <div className="text-lg md:text-2xl font-medium mb-5">{data.title}</div>
+      <div className="flex items-center justify-between">
+        <div className="flex flex-col">
+          <div className="flex items-center mb-1">
+            <div className="text-sm text-[#4F4F4F]">دسته بندی:</div>
+            <div className="text-[#333333] text-sm font-medium mr-1">
+              {data.cat}
+            </div>
+          </div>
+          <div className="flex items-center mb-1">
+            <div className="text-sm text-[#4F4F4F]">برند:</div>
+            <div className="text-[#333333] text-sm font-medium mr-1">
+              {data.brand}
+            </div>
+          </div>
+          <div className="flex items-center mb-1">
+            <div className="text-sm text-[#4F4F4F]">وضعیت کالا:</div>
+            <div>
+              {data.stock > 0 ? (
+                <span className="text-[#27AE60] text-sm font-medium mr-1">
+                  موجود
+                </span>
+              ) : (
+                <span className="text-[#DA0000] text-sm font-medium mr-1">
+                  ناموجود
+                </span>
+              )}
+            </div>
+          </div>
+        </div>
+      </div>
+      {data.features.map((item, index) => (
+        <div></div>
+      ))}
+    </div>
+  );
+};
+
+export default ProductDetail;
