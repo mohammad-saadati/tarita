e6494d49a9a824a0131e7b516e5b3450130da9fd
mohammad_saadati <saadati.m.b@gmail.com>
2023-06-25 20:42:15 +0330

    add rating component

--- src/components/ProductDetail/index.tsx
+++ src/components/ProductDetail/index.tsx
@@ -1,4 +1,6 @@
 import { FC } from "react";
+import { Rating, RoundedStar } from "@smastrom/react-rating";
+import "@smastrom/react-rating/style.css";
 
 type ProductDetailProps = {
   title: string;
@@ -8,13 +10,14 @@ type ProductDetailProps = {
   features: { title: string; value: number }[];
   colors: string[];
   sizes: { title: string; price: number }[];
+  rate: number;
 };
 
 const ProductDetail: FC<ProductDetailProps> = (data) => {
   return (
     <div>
       <div className="text-lg md:text-2xl font-medium mb-5">{data.title}</div>
-      <div className="flex items-center justify-between">
+      <div className="flex items-start justify-between">
         <div className="flex flex-col">
           <div className="flex items-center mb-1">
             <div className="text-sm text-[#4F4F4F]">دسته بندی:</div>
@@ -43,9 +46,24 @@ const ProductDetail: FC<ProductDetailProps> = (data) => {
             </div>
           </div>
         </div>
+        <div className="flex items-center">
+          <div className="ml-1">امتیاز:</div>
+          <Rating
+            className="max-w-[85px] md:max-w-[100px]"
+            value={data.rate}
+            readOnly
+            itemStyles={{
+              itemShapes: RoundedStar,
+              activeFillColor: "#F2C94C",
+              inactiveFillColor: "#E0E0E0",
+            }}
+          />
+        </div>
       </div>
       {data.features.map((item, index) => (
-        <div></div>
+        <div>
+          <div></div>
+        </div>
       ))}
     </div>
   );
