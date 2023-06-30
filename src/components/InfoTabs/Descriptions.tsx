5fffa91851d080ac1354b769bf8bebbe70f0b9fc
mohammad_saadati <saadati.m.b@gmail.com>
2023-06-30 22:54:48 +0330

    description component

--- src/components/InfoTabs/Descriptions.tsx
+++ src/components/InfoTabs/Descriptions.tsx
@@ -3,7 +3,7 @@ import { FC } from "react";
 type DescriptionProps = {};
 
 const Description: FC<DescriptionProps> = () => {
-  return <div></div>;
+  return <div>Description</div>;
 };
 
 export default Description;
