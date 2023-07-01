5a9d25d537aa704520fe9e55c442b0d83e092752
mohammad_saadati <saadati.m.b@gmail.com>
2023-07-01 23:47:49 +0330

    complete description

--- src/components/InfoTabs/Descriptions.tsx
+++ src/components/InfoTabs/Descriptions.tsx
@@ -8,15 +8,16 @@ type DescriptionProps = {
 const Description: FC<DescriptionProps> = ({ description }) => {
   return (
     <div>
-      <div className="text-sm leading-7">{description.text}</div>
+      <div className="text-sm leading-7 text-justify mb-5">{description.text}</div>
       <Image
-        className="float-right"
+        className="float-left mr-8 my-2"
         src={description.image}
         alt=""
         width={414}
         height={310}
         sizes="max-width(640px): 315px, 236px"
       />
+      <div className="text-sm leading-7 text-justify md:pl-55">{description.text}</div>
     </div>
   );
 };
