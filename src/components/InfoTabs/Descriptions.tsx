4f6c2c5136bd6ba79c92d5994a78d21e3fa95b61
mohammad_saadati <saadati.m.b@gmail.com>
2023-07-01 12:13:55 +0330

    description tab

--- src/components/InfoTabs/Descriptions.tsx
+++ src/components/InfoTabs/Descriptions.tsx
@@ -1,9 +1,24 @@
 import { FC } from "react";
+import Image from "next/image";
 
-type DescriptionProps = {};
+type DescriptionProps = {
+  description: { text: string; image: string };
+};
 
-const Description: FC<DescriptionProps> = () => {
-  return <div>Description</div>;
+const Description: FC<DescriptionProps> = ({ description }) => {
+  return (
+    <div>
+      <div className="text-sm leading-7">{description.text}</div>
+      <Image
+        className="float-right"
+        src={description.image}
+        alt=""
+        width={414}
+        height={310}
+        sizes="max-width(640px): 315px, 236px"
+      />
+    </div>
+  );
 };
 
 export default Description;
