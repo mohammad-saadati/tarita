444bc1fc4ee93bc20c9c3baf4b8ad384e8e7b883
mohammad_saadati <saadati.m.b@gmail.com>
2023-07-02 20:48:15 +0330

    show comments

--- src/components/InfoTabs/Comments.tsx
+++ src/components/InfoTabs/Comments.tsx
@@ -1,9 +1,52 @@
 import { FC } from "react";
+import Image from "next/image";
+import like from "@/assets/images/like.svg";
+import dislike from "@/assets/images/dislike.svg";
 
-type CommentsProps = {};
+type CommentsProps = {
+  comments: {
+    writer: string;
+    date: string;
+    text: string;
+    likes: number;
+    dislikes: number;
+  }[];
+};
 
-const Comments: FC<CommentsProps> = () => {
-  return <div>Comments</div>;
+const Comments: FC<CommentsProps> = ({ comments }) => {
+  return (
+    <div>
+      {comments.map((item, index) => (
+        <div className="flex flex-col border-b border-[#F2F2F2] pb-4 mb-4">
+          <div className="text-sm font-medium">{item.writer}</div>
+          <div className="text-xs text-[#828282] mb-3.5">{item.date}</div>
+          <div className="text-sm">{item.text}</div>
+          <div className="flex items-center justify-end mt-12">
+            <div className="flex items-center mr-12">
+              <span className="">{item.likes}</span>
+              <Image
+                className="cursor-pointer"
+                src={like}
+                alt=""
+                width="24"
+                height="24"
+              />
+            </div>
+            <div className="flex items-center mr-12">
+              <span className="">{item.dislikes}</span>
+              <Image
+                className="cursor-pointer"
+                src={dislike}
+                alt=""
+                width="24"
+                height="24"
+              />
+            </div>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default Comments;
