959c7e74225b54a604c4bd83596563931dbf006f
mohammad_saadati <saadati.m.b@gmail.com>
2023-07-03 22:35:48 +0330

    comment component

---
+++ src/components/Forms/CommentForm/index.tsx
@@ -0,0 +1,27 @@
+import { FC } from "react";
+
+type CommentFormProps = {};
+
+const CommentForm: FC<CommentFormProps> = () => {
+  return (
+    <div className="">
+      <form className="">
+        <textarea
+          placeholder="نظر خود را بنویسید"
+          className="bg-[#F2F2F2] w-full resize-none h-32 p-4 rounded-[10px]"
+          cols="20"
+        />
+        <div className="flex justify-end mt-5">
+          <button
+            type="submit"
+            className="w-[150px] h-[48px] bg-[#333] text-white rounded-[10px]"
+          >
+            ارسال نظر
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default CommentForm;
