6dccb1945b156550144b4ac9f198423917597155
mohammad_saadati <saadati.m.b@gmail.com>
2023-07-07 20:08:38 +0330

    header component

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,47 @@
+import React, { FC } from "react";
+import Image from "next/image";
+import userIcon from "@/assets/images/user.svg";
+import bagIcon from "@/assets/images/bag.svg";
+import searchNormal from "@/assets/images/search-normal.svg";
+
+type HeaderProps = {};
+
+const Header: FC<HeaderProps> = () => {
+  return (
+    <div className="bg-[#2B2B2B]">
+      <div className="container flex justify-between items-center h-[60px]">
+        <div className="text-white text-sm">فروشگاه آنلاین فرش</div>
+        <div className="relative">
+          <input className="bg-[#4A4A4A] rounded-[7px] h-[34px] text-[#D4D4D4] p-2 focus-visible:outline-none w-[15px] md:w-[400px]" />
+          <div className="absolute left-2 top-2">
+            <Image
+              src={searchNormal}
+              alt=""
+              width={20}
+              height={20}
+              className="cursor-pointer"
+            />
+          </div>
+        </div>
+        <div className="flex justify-center items-center">
+          <Image
+            src={userIcon}
+            alt="user"
+            width={20}
+            height={20}
+            className="cursor-pointer"
+          />
+          <Image
+            src={bagIcon}
+            alt="user"
+            width={20}
+            height={20}
+            className="mr-5 cursor-pointer"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
