a911654e61534a7e92f9aca3c4716989692293f5
mohammad_saadati <saadati.m.b@gmail.com>
2023-07-09 23:20:35 +0330

    show products

--- src/pages/search/index.tsx
+++ src/pages/search/index.tsx
@@ -2,21 +2,35 @@ import { GetServerSidePropsContext } from "next";
 import { NextPageWithLayout } from "../_app";
 import { identifier, LayoutTypes } from "@/components/layouts/layoutIdentifire";
 import Filters from "@/components/Filters";
+import Card from "@/components/Card";
 import axios from "@/utils/axios";
 
 interface SearchProps {
-  
+  products: {
+    id: number;
+    url: string;
+    title: string;
+    price: number;
+    rate: number;
+  }[];
 }
 
 const Search: NextPageWithLayout<SearchProps> = ({ data }) => {
+  console.log(data);
+
   return (
     <div className="grid grid-cols-12 gap-4">
-      <div className="col-span-12 md:col-span-9 lg:col-span-10 relative">
+      <div className="col-span-12 md:col-span-9 lg:col-span-2 relative">
         <Filters />
       </div>
-      <div className="col-span-12 md:col-span-9 lg:col-span-10 relative">
-        {}
-      </div>
+      {data.map((item, index) => (
+        <div
+          className="col-span-12 md:col-span-9 lg:col-span-2 relative"
+          key={index}
+        >
+          <Card item={item} />
+        </div>
+      ))}
     </div>
   );
 };
@@ -28,7 +42,7 @@ export async function getServerSideProps(context: GetServerSidePropsContext) {
   try {
     const res = await axios.get(`/products?_page=1&_limit=20`);
     const { data } = res;
-    console.log("***ddddd*", data, context.params?.id);
+    console.log("***ddddd*", data);
     return { props: { data } };
   } catch (error) {
     console.log(error);
