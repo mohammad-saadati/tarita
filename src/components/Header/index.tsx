e967eb2083a4f4b8ea677076b9d96e75ae2cc66b
mohammad_saadati <saadati.m.b@gmail.com>
2023-07-09 22:58:39 +0330

    typoraphy

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,5 +1,6 @@
 import React, { FC } from "react";
 import Image from "next/image";
+import Link from "next/link";
 import userIcon from "@/assets/images/user.svg";
 import bagIcon from "@/assets/images/bag.svg";
 import searchNormal from "@/assets/images/search-normal.svg";
@@ -10,7 +11,11 @@ const Header: FC<HeaderProps> = () => {
   return (
     <div className="bg-[#2B2B2B]">
       <div className="container flex justify-between items-center h-[60px]">
-        <div className="text-white text-[10px] md:text-sm">فروشگاه آنلاین فرش</div>
+        <div className="text-white font-bold text-[10px] md:text-sm">
+          <Link href="/" className="text-[12px]">
+            فروشگاه آنلاین فرش
+          </Link>
+        </div>
         <div className="relative">
           <input className="bg-[#4A4A4A] rounded-[7px] h-[34px] text-[#D4D4D4] p-2 focus-visible:outline-none w-[150px] md:w-[400px]" />
           <div className="absolute left-2 top-2">
