d0a0409d5683a6464a079163924512eba9a590ad
mohammad_saadati <saadati.m.b@gmail.com>
2023-07-09 23:04:53 +0330

    initial filters component

---
+++ src/components/Filters/index.tsx
@@ -0,0 +1,15 @@
+import React, { FC } from 'react';
+
+type FiltersProps = {
+  // Define props here
+};
+
+const Filters: FC<FiltersProps> = () => {
+  return (
+    <>
+      Filters
+    </>
+  );
+};
+
+export default Filters;
