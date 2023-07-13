a259710de762db2f8e98a5f839fbc84ae9bb2188
mohammad_saadati <saadati.m.b@gmail.com>
2023-07-13 22:39:47 +0330

    filters page

--- src/components/Filters/index.tsx
+++ src/components/Filters/index.tsx
@@ -1,14 +1,28 @@
-import React, { FC } from "react";
+import React, { FC, useState } from "react";
 
 type FiltersProps = {
   filters: { title: string; value: string }[];
+  title: string;
 };
 
-const Filters: FC<FiltersProps> = ({filters}) => {
+const Filters: FC<FiltersProps> = ({ filters }) => {
+  const [activeIndex, setActiveIndex] = useState(null);
+
   return (
-    <>
-      <div></div>
-    </>
+    <div>
+      {filters.map((item, index) => (
+        <div className="flex items-center">
+          <div>{item.title}</div>
+          <div
+            className={`w-[5px] h-[5px] ${
+              index === activeIndex
+                ? "bg-[#333333] border-[#333333]"
+                : "bg-[#F2F2F2] border-[#BDBDBD]"
+            }`}
+          ></div>
+        </div>
+      ))}
+    </div>
   );
 };
 
