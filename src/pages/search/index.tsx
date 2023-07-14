d84cca25d62ab371cbd9eac61c09b48177f959ee
mohammad_saadati <saadati.m.b@gmail.com>
2023-07-14 16:03:25 +0330

    increase gap

--- src/pages/search/index.tsx
+++ src/pages/search/index.tsx
@@ -33,7 +33,7 @@ const Search: NextPageWithLayout<SearchProps> = ({ products, filters }) => {
         </div>
       </div>
       <div className="col-span-12 md:col-span-9 lg:col-span-9">
-        <div className="grid grid-cols-12 gap-4">
+        <div className="grid grid-cols-12 gap-8">
           {products.map((item, index) => (
             <div
               className="col-span-12 md:col-span-9 lg:col-span-3 relative"
