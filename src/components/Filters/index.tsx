106c735c25f765bd3f302bc6dee332c4ccbded9f
mohammad_saadati <saadati.m.b@gmail.com>
2023-07-14 17:07:08 +0330

    use scss module

--- src/components/Filters/index.tsx
+++ src/components/Filters/index.tsx
@@ -2,6 +2,7 @@ import React, { FC, useState } from "react";
 import Image from "next/image";
 import plus from "@/assets/images/plus.svg";
 import minus from "@/assets/images/minus.svg";
+import styles from "./filters.module.scss";
 
 type FiltersProps = {
   filters: { title: string; value: string }[];
@@ -13,10 +14,13 @@ const Filters: FC<FiltersProps> = ({ filters, title }) => {
   const [isCollapsed, setIsCollapsed] = useState(false);
 
   return (
-    <div className="border border-[#E0E0E0] rounded-[10px] py-[15px]">
+    <div className="border border-[#E0E0E0] rounded-[10px] pt-[15px]">
       <div className="flex items-center justify-between text-sm font-semibold mb-[15px] border-b px-[15px] pb-[15px]">
         <div>{title}</div>
-        <div>
+        <div
+          className="cursor-pointer"
+          onClick={() => setIsCollapsed(!isCollapsed)}
+        >
           {isCollapsed ? (
             <Image src={plus} alt="plus" width={20} height={20} />
           ) : (
@@ -24,18 +28,20 @@ const Filters: FC<FiltersProps> = ({ filters, title }) => {
           )}
         </div>
       </div>
-      {filters.map((item, index) => (
-        <div className="flex justify-between items-center px-[15px] pb-[15px]">
-          <div className="text-[13px]">{item.title}</div>
-          <div
-            className={`w-[15px] h-[15px] rounded border ${
-              index === activeIndex
-                ? "bg-[#333333] border-[#333333]"
-                : "bg-[#F2F2F2] border-[#BDBDBD]"
-            }`}
-          ></div>
-        </div>
-      ))}
+      <div className={`${styles.filtersWrapper} ${isCollapsed ? styles.isCollapsed : ""}`}>
+        {filters.map((item, index) => (
+          <div className="flex justify-between items-center px-[15px] pb-[15px]">
+            <div className="text-[13px]">{item.title}</div>
+            <div
+              className={`w-[15px] h-[15px] rounded border ${
+                index === activeIndex
+                  ? "bg-[#333333] border-[#333333]"
+                  : "bg-[#F2F2F2] border-[#BDBDBD]"
+              }`}
+            ></div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
