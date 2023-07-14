df238c4930b6e74b77caacb52df1ae15d61b334c
mohammad_saadati <saadati.m.b@gmail.com>
2023-07-14 20:15:09 +0330

    initial range component

---
+++ src/components/Filters/range.tsx
@@ -0,0 +1,16 @@
+import React, { FC } from "react";
+import Filters from "@/components/Filters";
+
+type RangeProps = {
+  // Define props here
+};
+
+const Range: FC<RangeProps> = () => {
+  return (
+    <div>
+      <Filters />
+    </div>
+  );
+};
+
+export default Range;
