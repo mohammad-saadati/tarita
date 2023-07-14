f27c63a0282bc1fb5f85dc6765436589cf1c93be
mohammad_saadati <saadati.m.b@gmail.com>
2023-07-14 16:03:15 +0330

    conditional rendering icon

--- src/components/Filters/index.tsx
+++ src/components/Filters/index.tsx
@@ -10,21 +10,25 @@ type FiltersProps = {
 
 const Filters: FC<FiltersProps> = ({ filters, title }) => {
   const [activeIndex, setActiveIndex] = useState(null);
+  const [isCollapsed, setIsCollapsed] = useState(false);
 
   return (
     <div className="border border-[#E0E0E0] rounded-[10px] py-[15px]">
       <div className="flex items-center justify-between text-sm font-semibold mb-[15px] border-b px-[15px] pb-[15px]">
         <div>{title}</div>
         <div>
-          <Image src={plus} alt="plus" width={10} height={15} />
-          <Image src={minus} alt="minus" width={10} height={15} />
+          {isCollapsed ? (
+            <Image src={plus} alt="plus" width={20} height={20} />
+          ) : (
+            <Image src={minus} alt="minus" width={20} height={20} />
+          )}
         </div>
       </div>
       {filters.map((item, index) => (
         <div className="flex justify-between items-center px-[15px] pb-[15px]">
           <div className="text-[13px]">{item.title}</div>
           <div
-            className={`w-[5px] h-[5px] ${
+            className={`w-[15px] h-[15px] rounded border ${
               index === activeIndex
                 ? "bg-[#333333] border-[#333333]"
                 : "bg-[#F2F2F2] border-[#BDBDBD]"
