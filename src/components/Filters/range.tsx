4c6a6628a54ce937d52d6c0b6e68d5d22cd98987
mohammad_saadati <saadati.m.b@gmail.com>
2023-07-16 22:19:53 +0330

    set props for range component

--- src/components/Filters/range.tsx
+++ src/components/Filters/range.tsx
@@ -10,11 +10,21 @@ type RangeProps = {
 };
 
 const Range: FC<RangeProps> = () => {
+  const handleRangeUpdate = (value: [number, number]) => {
+    console.log(value);
+  };
+
   return (
     <div>
       <Filters title="محدوده قیمت">
         <div className="border-t py-8 px-5">
-          <RangeSlider id="range-slider-input-component"/>
+          <RangeSlider
+            id="range-slider-input-component"
+            min={1000000}
+            max={1000000000}
+            step={10000000}
+            onInput={handleRangeUpdate}
+          />
           <div className="flex flex-wrap justify-between items-center">
             <div className="border p-2 rounded-[7px] mt-8 text-sm">از</div>
           </div>
