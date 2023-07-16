7181acc08149f4998b73307e64f7a38e0641ffde
mohammad_saadati <saadati.m.b@gmail.com>
2023-07-16 22:09:31 +0330

    set styles

--- src/components/Filters/range.tsx
+++ src/components/Filters/range.tsx
@@ -1,6 +1,7 @@
 import React, { FC } from "react";
 import RangeSlider from "react-range-slider-input";
 import "react-range-slider-input/dist/style.css";
+import "./range.scss";
 
 import Filters from "@/components/Filters";
 
@@ -12,7 +13,12 @@ const Range: FC<RangeProps> = () => {
   return (
     <div>
       <Filters title="محدوده قیمت">
-        <div className="border-t py-8 px-5"><RangeSlider /></div>
+        <div className="border-t py-8 px-5">
+          <RangeSlider id="range-slider-input-component"/>
+          <div className="flex flex-wrap justify-between items-center">
+            <div className="border p-2 rounded-[7px] mt-8 text-sm">از</div>
+          </div>
+        </div>
       </Filters>
     </div>
   );
