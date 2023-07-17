4eb3b35e1302e6f145e3e55aed7f91a90e067056
mohammad_saadati <saadati.m.b@gmail.com>
2023-07-17 13:12:25 +0330

    redux search slice

---
+++ src/store/searchSlice.ts
@@ -0,0 +1,23 @@
+import { createSlice } from "@reduxjs/toolkit";
+import type { PayloadAction } from "@reduxjs/toolkit";
+
+interface SearchState {
+  search: string;
+}
+
+const initialState: SearchState = {
+  search: "",
+};
+
+const searchState = createSlice({
+  name: "search",
+  initialState,
+  reducers: {
+    setSearch(state, action: PayloadAction<string>) {
+      state.search = action.payload;
+    },
+  },
+});
+
+export const { setSearch } = searchState.actions;
+export default searchState.reducer;
