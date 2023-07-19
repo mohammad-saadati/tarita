1a5e25dcf0f63d4243432483b14a8e6d12dde078
mohammad_saadati <saadati.m.b@gmail.com>
2023-07-19 13:26:44 +0330

    register currentuser slice

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,12 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 // reducers
-import searchReducer from "./features/searchSlice";
+import searchReducer from "./features/search";
+import currentUserReducer from "./features/currentUser";
 
 export const store = configureStore({
   reducer: {
     search: searchReducer,
+    currentUser: currentUserReducer,
   },
 });
 
 export type RootState = ReturnType<typeof store.getState>;
-export type AppDispatch = typeof store.dispatch ;
+export type AppDispatch = typeof store.dispatch;
