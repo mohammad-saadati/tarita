c85873b8ac2eee48906899b2d884f4515f6b6633
mohammad_saadati <saadati.m.b@gmail.com>
2023-07-24 12:07:59 +0330

    axios base url

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -2,7 +2,8 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
   // https://fake-api-roan.vercel.app/api/
-  baseURL: "http://localhost:3001",
+  // http://localhost:3001
+  baseURL: "https://fake-api-roan.vercel.app/api/",
   headers: {},
 });
 
