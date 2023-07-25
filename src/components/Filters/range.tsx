eeae90e356d839ef46f847cbc397a82da994052a
mohammad_saadati <saadati.m.b@gmail.com>
2023-07-26 01:27:16 +0330

    useEffect to reset range filter

--- src/components/Filters/range.tsx
+++ src/components/Filters/range.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useState } from "react";
+import React, { FC, useState, useEffect } from "react";
 // @ts-ignore
 import RangeSlider from "react-range-slider-input";
 import "react-range-slider-input/dist/style.css";
@@ -13,14 +13,21 @@ type RangeProps = {
 };
 
 const Range: FC<RangeProps> = () => {
-  const [min, setMin] = useState(1000000);
-  const [max, setMax] = useState(1000000000);
+  const [min, setMin] = useState(200000000);
+  const [max, setMax] = useState(800000000);
   const router = useRouter();
 
   const handleRangeUpdate = (value: [number, number]) => {
     setMin(value[0]);
     setMax(value[1]);
   };
+
+  useEffect(() => {
+    if (Object.entries(router.query).length === 0) {
+      setMin(200000000);
+      setMax(800000000);
+    }
+  }, [router.query]);
 
   const activePriceRange = () => {
     const queryParams = new URLSearchParams();
@@ -59,6 +66,7 @@ const Range: FC<RangeProps> = () => {
             step={10000000}
             onInput={handleRangeUpdate}
             rangeSlideDisabled={true}
+            value={[min, max]}
           />
           <div className="flex flex-wrap justify-between items-center mt-8 text-sm">
             <div className="flex flex-wrap justify-between items-center text-[10px] border border-[#E0E0E0] rounded-[7px] p-2">
