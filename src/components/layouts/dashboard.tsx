832e9a70070a391c3441b2c184f8c4d2dad7a590
mohammad_saadati <saadati.m.b@gmail.com>
2023-07-28 21:25:12 +0330

    dashboard layout

--- src/components/layouts/dashboard.tsx
+++ src/components/layouts/dashboard.tsx
@@ -1,4 +1,8 @@
 import type { ReactElement } from "react";
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
+import Sidebar from "@/components/Sidebar";
+import StoreProvider from "@/components/Providers/store";
 
 type layoutProps = {
   children: ReactElement;
@@ -7,8 +11,22 @@ type layoutProps = {
 const Dashboard = ({ children }: layoutProps) => {
   return (
     <>
-      <div>dashboard layout</div>
-      {children}
+      <div>
+        <StoreProvider>
+          <Header />
+          <div className="container my-[70px] md:my-[100px]">
+            <div className="grid grid-cols-12 gap-4">
+              <div className="col-span-12 md:col-span-9 lg:col-span-3">
+                <Sidebar />
+              </div>
+              <div className="col-span-12 md:col-span-9 lg:col-span-9">
+                {children}
+              </div>
+            </div>
+          </div>
+          <Footer />
+        </StoreProvider>
+      </div>
     </>
   );
 };
