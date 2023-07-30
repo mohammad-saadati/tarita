b9f4508fbd9234f5e1dc8c193da40455b956c8b5
mohammad_saadati <saadati.m.b@gmail.com>
2023-07-30 22:09:16 +0330

    bookmarks section in dashboard page

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -2,21 +2,39 @@ import Link from "next/link";
 import { NextPageWithLayout } from "../_app";
 import { ReactElement } from "react";
 import { identifier, LayoutTypes } from "@/components/layouts/layoutIdentifire";
-import Sidebar from "@/components/Sidebar";
 import Orders from "@/components/Orders";
+import Card from "@/components/Card";
+// store
+import { store } from "@/store";
+import { setCurrentUser, User } from "@/store/features/currentUser";
 
 interface DashboardProps {}
 
 const Dashboard: NextPageWithLayout<DashboardProps> = () => {
+  const bookmarks = store.getState().currentUser.user.bookmarked;
+
   return (
     <div>
       <div className="flex justify-between items-center mb-6">
-        <div className="text-sm">سفارش ها</div>
+        <div className="text-sm font-medium">سفارش ها</div>
         <div className="text-sm cursor-pointer">
           <Link href="/orders">مشاهده همه</Link>
         </div>
       </div>
       <Orders />
+      <div className="flex justify-between items-center mb-6 mt-16">
+        <div className="text-sm font-medium">علاقه مندی ها</div>
+        <div className="text-sm cursor-pointer">
+          <Link href="/orders">مشاهده همه</Link>
+        </div>
+      </div>
+      <div className="grid grid-cols-12 gap-4">
+        {bookmarks.map((item, index) => (
+          <div className="col-span-12 sm:col-span-6 md:col-span-6 lg:col-span-3">
+            <Card item={item} isLieked={true} />
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
