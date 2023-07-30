a6bf2ef7464640edaebe0f0580c22ba898e74a8c
mohammad_saadati <saadati.m.b@gmail.com>
2023-07-30 22:10:04 +0330

    add product type

--- src/store/features/currentUser.ts
+++ src/store/features/currentUser.ts
@@ -4,15 +4,25 @@ export interface User {
   id: string;
   name: string;
   avatar: string;
+  bookmarked: Product[];
 }
 interface currentUserState {
   user: User;
+}
+interface Product {
+  id: number;
+  url: string;
+  title: string;
+  price: number;
+  rate: number;
+  categories: string;
 }
 const initialState: currentUserState = {
   user: {
     id: "",
     name: "",
     avatar: "",
+    bookmarked: [],
   },
 };
 
