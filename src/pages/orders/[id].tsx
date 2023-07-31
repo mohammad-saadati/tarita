1339ba06bc5554ebf55cdb8a385136e84f4b9790
mohammad_saadati <saadati.m.b@gmail.com>
2023-08-01 01:18:51 +0330

    order detail page

---
+++ src/pages/orders/[id].tsx
@@ -0,0 +1,44 @@
+import { NextPageWithLayout } from "./_app";
+import { identifier, LayoutTypes } from "@/components/layouts/layoutIdentifire";
+//
+import OrdersDetail from "@/components/OrdersDetail";
+//
+import axios from "@/utils/axios";
+import { ReactElement } from "react";
+
+interface OrdersProps {
+  data: {
+    id: number;
+    url: string;
+    title: string;
+    price: number;
+    rate: number;
+    category: string;
+  };
+}
+
+const Orders: NextPageWithLayout<OrdersProps> = ({}) => {
+  return (
+    <>
+      <OrdersDetail />
+    </>
+  );
+};
+
+Orders.getLayout = (page: ReactElement) =>
+  identifier(page, LayoutTypes.dashboard);
+
+export async function getServerSideProps() {
+  // try {
+  //   const res = await axios.get("/orders");
+  //   const { data } = res;
+  //   console.log("***ddddd*", data);
+  //   return { props: { data } };
+  // } catch (error) {
+  //   console.log(error);
+  // }
+
+  return { props: { data: {} } };
+}
+
+export default Orders;
