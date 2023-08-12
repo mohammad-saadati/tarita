92b108df26488a6a24bba43780d47dcecc3730ee
mohammad_saadati <saadati.m.b@gmail.com>
2023-08-12 16:17:44 +0330

    cart page reactivity

--- src/pages/cart/index.tsx
+++ src/pages/cart/index.tsx
@@ -1,9 +1,11 @@
 import Image from "next/image";
-import { ReactElement } from "react";
+import { ReactElement, useState } from "react";
 import { NextPageWithLayout } from "../_app";
 import { identifier, LayoutTypes } from "@/components/layouts/layoutIdentifire";
 import { store } from "@/store";
 import { commaSeperator } from "@/utils/helpers";
+import { setCardItemCount } from "@/store/features/currentUser";
+import { useAppDispatch, useAppSelector } from "@/store/hooks";
 import plusWhite from "@/assets/images/plus-white.svg";
 import minusWhite from "@/assets/images/minusWhite.svg";
 import removeCircle from "@/assets/images/removeCircle.svg";
@@ -18,8 +20,14 @@ interface CartProps {
 }
 
 const Cart: NextPageWithLayout<CartProps> = ({ data }) => {
-  const cartItems = store.getState().currentUser.user.cartItems;
-  const addresses = store.getState().currentUser.user.addresses;
+  const dispatch = useAppDispatch();
+  const cartItems = useAppSelector((store) => store.currentUser.user.cartItems);
+  const addresses = useAppSelector((store) => store.currentUser.user.cartItems);
+  // const cartItems = store.getState().currentUser.user.cartItems;
+  // const addresses = store.getState().currentUser.user.addresses;
+  const [paymentMethod, setPaymentMethod] = useState(0);
+  const [shippingType, setShippingType] = useState(0);
+  const [AddressNumber, setAddressNumber] = useState(0);
 
   return (
     <>
@@ -64,15 +72,29 @@ const Cart: NextPageWithLayout<CartProps> = ({ data }) => {
                       </div>
                     </div>
                   </div>
-                  <div className="flex justify-between items-center mr-[90px] mt-5">
+                  <div className="flex justify-between items-center mr-[15px] md:mr-[90px] mt-5">
                     <div className="flex justify-between items-center">
-                      <div className="flex items-center justify-center bg-[#333] w-[32px] h-[32px] rounded-[8px]">
+                      <div
+                        className="flex items-center justify-center bg-[#333] w-[32px] h-[32px] rounded-[8px] cursor-pointer"
+                        onClick={() => {
+                          dispatch(
+                            setCardItemCount({ index, value: item.count + 1 })
+                          );
+                        }}
+                      >
                         <Image src={plusWhite} width={12} height={12} alt="" />
                       </div>
                       <div className="flex items-center justify-center mx-2 border rounded-[8px] w-[46px] h-[32px]">
                         {item.count}
                       </div>
-                      <div className="flex items-center justify-center bg-[#333] w-[32px] h-[32px] rounded-[8px]">
+                      <div
+                        className="flex items-center justify-center bg-[#333] w-[32px] h-[32px] rounded-[8px] cursor-pointer"
+                        onClick={() => {
+                          dispatch(
+                            setCardItemCount({ index, value: item.count - 1 })
+                          );
+                        }}
+                      >
                         <Image src={minusWhite} width={12} height={12} alt="" />
                       </div>
                     </div>
@@ -132,10 +154,19 @@ const Cart: NextPageWithLayout<CartProps> = ({ data }) => {
           <div className="">
             {addresses.map((address, index) => (
               <div className="mt-10" key={index}>
-                <div className="border-[#F2F2F2] border-[2px] p-3.5 rounded-[7px]">
+                <div
+                  onClick={() => {
+                    setAddressNumber(index);
+                  }}
+                  className={`border-[2px] p-3.5 rounded-[7px] cursor-pointer ${
+                    AddressNumber == index
+                      ? "border-[#333]"
+                      : "border-[#F2F2F2]"
+                  }`}
+                >
                   <div className="flex justify-between items-center">
                     <div className="font-bold text-sm">{address.name}</div>
-                    {address.isActive ? (
+                    {AddressNumber == index ? (
                       <Image
                         src={checkedCircle}
                         width={20}
@@ -161,8 +192,15 @@ const Cart: NextPageWithLayout<CartProps> = ({ data }) => {
             <div className="text-sm font-bold">انتخاب شیوه ارسال</div>
           </div>
           <div className="grid grid-cols-12 gap-4">
-            <div className="col-span-6">
-              <div className="border-[#F2F2F2] border-[2px] p-3.5 rounded-[7px]">
+            <div
+              onClick={() => setShippingType(0)}
+              className="col-span-12 md:col-span-6 cursor-pointer"
+            >
+              <div
+                className={`border-[2px] p-3.5 rounded-[7px] ${
+                  shippingType === 0 ? "border-[#333]" : "border-[#F2F2F2]"
+                }`}
+              >
                 <div className="flex items-center">
                   <Image
                     src={certifiedMail}
@@ -173,12 +211,14 @@ const Cart: NextPageWithLayout<CartProps> = ({ data }) => {
                   <div className="flex flex-col w-full mr-2">
                     <div className="flex justify-between items-center w-full mb-3">
                       <div className="text-sm font-medium">پست پیشتاز</div>
-                      {/* <Image
-                        src={checkedCircle}
-                        width={20}
-                        height={20}
-                        alt=""
-                      /> */}
+                      {shippingType === 0 ? (
+                        <Image
+                          src={checkedCircle}
+                          width={20}
+                          height={20}
+                          alt=""
+                        />
+                      ) : null}
                     </div>
                     <div className="flex justify-between items-center w-full">
                       <div className="text-sm">3 تا 5 روز کاری</div>
@@ -191,8 +231,15 @@ const Cart: NextPageWithLayout<CartProps> = ({ data }) => {
                 </div>
               </div>
             </div>
-            <div className="col-span-6">
-              <div className="border-[#F2F2F2] border-[2px] p-3.5 rounded-[7px]">
+            <div
+              onClick={() => setShippingType(1)}
+              className="col-span-12 md:col-span-6 cursor-pointer"
+            >
+              <div
+                className={`border-[2px] p-3.5 rounded-[7px] ${
+                  shippingType === 1 ? "border-[#333]" : "border-[#F2F2F2]"
+                }`}
+              >
                 <div className="flex items-center">
                   <Image
                     src={certifiedMail}
@@ -203,12 +250,14 @@ const Cart: NextPageWithLayout<CartProps> = ({ data }) => {
                   <div className="flex flex-col w-full mr-2">
                     <div className="flex justify-between items-center w-full mb-3">
                       <div className="text-sm font-medium">پست سفارشی</div>
-                      <Image
-                        src={checkedCircle}
-                        width={20}
-                        height={20}
-                        alt=""
-                      />
+                      {shippingType === 1 ? (
+                        <Image
+                          src={checkedCircle}
+                          width={20}
+                          height={20}
+                          alt=""
+                        />
+                      ) : null}
                     </div>
                     <div className="flex justify-between items-center w-full">
                       <div className="text-sm">3 تا 5 روز کاری</div>
@@ -228,8 +277,15 @@ const Cart: NextPageWithLayout<CartProps> = ({ data }) => {
             <div className="text-sm font-bold">روش پرداخت سفارش</div>
           </div>
           <div className="grid grid-cols-12 gap-4">
-            <div className="col-span-6">
-              <div className="border-[#F2F2F2] border-[2px] p-3.5 rounded-[7px]">
+            <div
+              onClick={() => setPaymentMethod(0)}
+              className="col-span-12 md:col-span-6 cursor-pointer"
+            >
+              <div
+                className={`border-[2px] p-3.5 rounded-[7px] ${
+                  paymentMethod === 0 ? "border-[#333]" : "border-[#F2F2F2]"
+                }`}
+              >
                 <div className="flex justify-between items-center">
                   <div className="flex justify-between items-center">
                     <Image src={mellat} width={45} height={45} alt="mellat" />
@@ -237,12 +293,21 @@ const Cart: NextPageWithLayout<CartProps> = ({ data }) => {
                       پرداخت انلاین بانک ملت
                     </div>
                   </div>
-                  <Image src={checkedCircle} width={20} height={20} alt="" />
+                  {paymentMethod === 0 ? (
+                    <Image src={checkedCircle} width={20} height={20} alt="" />
+                  ) : null}
                 </div>
               </div>
             </div>
-            <div className="col-span-6">
-              <div className="border-[#F2F2F2] border-[2px] p-3.5 rounded-[7px]">
+            <div
+              onClick={() => setPaymentMethod(1)}
+              className="col-span-12 md:col-span-6 cursor-pointer"
+            >
+              <div
+                className={`border-[2px] p-3.5 rounded-[7px] ${
+                  paymentMethod === 1 ? "border-[#333]" : "border-[#F2F2F2]"
+                }`}
+              >
                 <div className="flex justify-between items-center">
                   <div className="flex justify-between items-center">
                     <Image src={saman} width={45} height={45} alt="mellat" />
@@ -250,18 +315,36 @@ const Cart: NextPageWithLayout<CartProps> = ({ data }) => {
                       پرداخت انلاین بانک ملت
                     </div>
                   </div>
+                  {paymentMethod === 1 ? (
+                    <Image src={checkedCircle} width={20} height={20} alt="" />
+                  ) : null}
                 </div>
               </div>
             </div>
-            <div className="col-span-6">
-              <div className="border-[#F2F2F2] border-[2px] p-3.5 rounded-[7px]">
+            <div
+              onClick={() => setPaymentMethod(2)}
+              className="col-span-12 md:col-span-6 cursor-pointer"
+            >
+              <div
+                className={`border-[2px] p-3.5 rounded-[7px] ${
+                  paymentMethod === 2 ? "border-[#333]" : "border-[#F2F2F2]"
+                }`}
+              >
                 <div className="flex justify-between items-center">
                   <div className="flex justify-between items-center">
-                    <Image src={cardToCard} width={45} height={45} alt="mellat" />
+                    <Image
+                      src={cardToCard}
+                      width={45}
+                      height={45}
+                      alt="mellat"
+                    />
                     <div className="flex flex-col text-sm font-medium w-full mr-2">
                       پرداخت انلاین بانک ملت
                     </div>
                   </div>
+                  {paymentMethod === 2 ? (
+                    <Image src={checkedCircle} width={20} height={20} alt="" />
+                  ) : null}
                 </div>
               </div>
             </div>
