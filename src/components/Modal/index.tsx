aeb24d9da109c244d43600e40207cb365bb8f004
mohammad_saadati <saadati.m.b@gmail.com>
2023-08-13 21:58:21 +0330

    modal template

---
+++ src/components/Modal/index.tsx
@@ -0,0 +1,22 @@
+import { React, FC } from "react";
+
+interface ModalProps {
+  children: React.ReachElement;
+}
+const Modal: FC<ModalProps> = ({ children }) => {
+  return (
+    <div
+      className="fixed inset-0 flex items-center justify-center bg-black opacity-100 w-screen h-screen"
+      style={{ background: "rgba(0, 0, 0, 0.40)", backdropFilter: "blur(5px)" }}
+    >
+      <div className="fixed bg-white opacity-50 w-screen h-screen z-50"></div>
+      <div className="bg-white p-5 rounded-[10px]">
+        <div className="z-50">title</div>
+        <div className="z-50">dddddddddddddd</div>
+        <div className="z-50">{children}</div>
+      </div>
+    </div>
+  );
+};
+
+export default Modal;
