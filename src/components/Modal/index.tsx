d1cb7c7b83a224c3349e24b5d2b22daa77f670b1
mohammad_saadati <saadati.m.b@gmail.com>
2023-08-22 10:43:46 +0330

    add props to modal

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -2,18 +2,22 @@ import React, { FC } from "react";
 
 interface ModalProps {
   children: React.ReactElement;
+  close: () => void;
+  title?: string;
 }
-const Modal: FC<ModalProps> = ({ children }) => {
+const Modal: FC<ModalProps> = ({ children, close, title }) => {
   return (
     <div
-      className="fixed inset-0 flex items-center justify-center bg-black opacity-100 w-screen h-screen"
+      className="fixed inset-0 flex items-center justify-center bg-black opacity-100 w-screen h-screen z-10"
       style={{ background: "rgba(0, 0, 0, 0.40)", backdropFilter: "blur(5px)" }}
     >
-      <div className="fixed bg-white opacity-50 w-screen h-screen z-50"></div>
-      <div className="bg-white p-5 rounded-[10px]">
-        <div className="z-50">title</div>
-        <div className="z-50">dddddddddddddd</div>
-        <div className="z-50">{children}</div>
+      <div
+        onClick={close}
+        className="fixed bg-white opacity-50 w-screen h-screen"
+      ></div>
+      <div className="bg-white p-5 rounded-[10px] w-11/12 h-fit z-20">
+        <div className="">{title}</div>
+        <div className="flex flex-wrap">{children}</div>
       </div>
     </div>
   );
