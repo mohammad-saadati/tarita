3e94eb3e75829792a0dfa38af8a237457a08162f
mohammad_saadati <saadati.m.b@gmail.com>
2023-08-25 12:03:53 +0330

    add close button to modal

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -1,11 +1,14 @@
+import Image from "next/image";
 import React, { FC } from "react";
+import closeIcom from "@/assets/images/close.svg";
 
 interface ModalProps {
   children: React.ReactElement;
-  close: () => void;
+  close?: () => void;
   title?: string;
+  showClose?: boolean;
 }
-const Modal: FC<ModalProps> = ({ children, close, title }) => {
+const Modal: FC<ModalProps> = ({ children, close, title, showClose }) => {
   return (
     <div
       className="fixed inset-0 flex items-center justify-center bg-black opacity-100 w-screen h-screen z-10"
@@ -15,9 +18,19 @@ const Modal: FC<ModalProps> = ({ children, close, title }) => {
         onClick={close}
         className="fixed bg-white opacity-50 w-screen h-screen"
       ></div>
-      <div className="bg-white p-5 rounded-[10px] w-11/12 h-fit z-20">
+      <div className="bg-white p-5 rounded-[10px] w-11/12 lg:w-auto h-fit z-20">
+        {showClose ? (
+          <div className="mb-6">
+            <Image
+              onClick={() => close?.()}
+              className="inline cursor-pointer"
+              src={closeIcom}
+              alt="close-modal"
+            />
+          </div>
+        ) : null}
         <div className="">{title}</div>
-        <div className="flex flex-wrap">{children}</div>
+        <div className="">{children}</div>
       </div>
     </div>
   );
